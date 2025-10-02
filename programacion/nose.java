c3580967bef2513ebfbeda236a95fc2e9929ce3a
hector6933 <hcriadog@iesch.org>
2025-10-02 15:51:34 +0200

    Creada la carpeta de... Lenguaje de marcas

---
+++ programacion/nose.java
@@ -0,0 +1,25 @@
+package programacion;
+import java.util.*;
+public class nose {
+    
+ public static void main(String[] args) {
+     
+ 
+    System.out.println("Dime tu edad");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+
+   
+    if (a > 116 || a < 0) {
+         System.out.println("Mentirosin");
+    } else
+        System.out.println("que ");
+    }
+
+   
+    Scanner leer = new Scanner(System.in);
+    Int b = leer.nextInt();
+
+ }
+
+
