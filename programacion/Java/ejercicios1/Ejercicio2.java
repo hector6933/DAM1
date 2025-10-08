439ef61a8cb2e88dc98397ee833b0e2aaeaa38b6
hector6933 <hcriadog@iesch.org>
2025-10-08 17:39:16 +0200

    Mas ejercicios de Java

---
+++ programacion/Java/ejercicios1/Ejercicio2.java
@@ -0,0 +1,11 @@
+package ejercicios1;
+
+public class Ejercicio2 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("El area de un circulo con radio 5,2cm es de: " + 3.14 * Math.pow(5.2, 2));
+
+    }
+
+}
---
+++ programacion/Java/ejercicios1/Ejercicio4.java
@@ -0,0 +1,18 @@
+package ejercicios1;
+
+public class Ejercicio4 {
+  
+    public static void main(String[] args) {
+        
+        int a = 20;
+        int b = 30;
+        int c;
+
+        c = b;
+        b = a;
+        a = c;
+
+
+    }
+
+}
