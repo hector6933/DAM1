911141f8a14b05e88b315967a2b96c3867ce96cf
Andres <aastan@iesch.org>
2025-10-10 19:42:18 +0200

    Add files via upload

---
+++ Andres/Programacion/Lectura.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Lectura {
+
+    public static void main(String[] args) {
+        
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Introduce un numero entero:");
+        int numeroentero = sc.nextInt();
+        double numerodouble = (double) numeroentero;
+        System.out.println("El numero como double es:" + numerodouble);
+       
+
+    
+        System.out.println("Dime el numero");
+        int numero = sc.nextInt();
+        System.out.println("El numero multiplicado por dos es:" +  numero * 2);
+        
+        
+        sc.close();
+    }
+    
+}
