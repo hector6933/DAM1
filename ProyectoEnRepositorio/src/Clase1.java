5944f62602012260f0e0112b467897453d238bf7
hector6933 <hcriadog@iesch.org>
2025-10-10 20:18:21 +0200

    Commit desde intelligj njbaisbdabda

---
+++ ProyectoEnRepositorio/src/Clase1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Clase1 {
+
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+        System.out.print("");
+
+    }
+
+}
