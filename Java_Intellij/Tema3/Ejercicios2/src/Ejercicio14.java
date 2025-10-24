509498e8913a0e7e01e647c06fef6118bc688d64
hector6933 <hcriadog@iesch.org>
2025-10-24 18:25:16 +0200

    Más ejercicios de java

---
+++ Java_Intellij/Tema3/Ejercicios2/src/Ejercicio14.java
@@ -0,0 +1,18 @@
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Múltiplos de 3:");
+        for (int i = 1; i < 51; i++) {
+
+            if (i % 3 == 0) {
+
+                System.out.print(i + " ");
+
+            }
+
+        }
+
+    }
+
+}
