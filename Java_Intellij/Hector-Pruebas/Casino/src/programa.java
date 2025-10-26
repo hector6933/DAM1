8b29160464b75a1ad4bd981b060d0fa139efbc9a
hector6933 <hcriadog@iesch.org>
2025-10-26 21:21:30 +0100

    Terminado el blackJack del HecSino

--- Java_Intellij/Hector-Pruebas/Casino/src/programa.java
+++ Java_Intellij/Hector-Pruebas/Casino/src/programa.java
@@ -18,6 +18,12 @@ public class programa {
 
     }
 
+    public static int random (int max, int min) {
+
+        return (int) (Math.random() * (max - min) + min);
+
+    }
+
     public static String redondear (double saldo) {
 
         if (saldo == Math.floor(saldo)) {
@@ -153,11 +159,17 @@ public class programa {
     public static double ruleta(double saldo) {
 
         boolean salir;
-        double apuesta;
-        int resultado;
+        double apuesta = 0;
+        int resultado = 99;
         String opcion;
+        System.out.println("• Bienvenido a la ruleta •");
+        esperar(0.4);
+        System.out.print("Donde el destino, ");
+        esperar(1.2);
+        System.out.println("elige su fortuna.");
+        esperar(0.7);
         System.out.println("-------------------------------");
-        System.out.println("Demomento solo disponemos de una ruleta con rojo, negro o verde");
+        System.out.println("De momento solo disponemos de una ruleta con rojo, negro o verde.");
         esperar(0.7);
         do {
 
@@ -174,16 +186,18 @@ public class programa {
                 salir = true;
                 break;
 
+            } else if (opcion.equalsIgnoreCase("1") || opcion.equalsIgnoreCase("2") || opcion.equalsIgnoreCase("3")) {
+
+                apuesta = apuesta(saldo);
+                if (apuesta == 0) {
+
+                    continue;
+
+                }
+                saldo = saldo - apuesta;
+                resultado = ruletaRojoNegro();
+
             }
-
-            apuesta = apuesta(saldo);
-            if (apuesta == 0) {
-
-                continue;
-
-            }
-            saldo = saldo - apuesta;
-            resultado = ruletaRojoNegro();
 
             switch (opcion) {
 
@@ -191,7 +205,7 @@ public class programa {
                     if (resultado == 0) {
 
                         System.out.println("HA GANADO !!!");
-                        saldo = apuesta * 2;
+                        saldo = saldo + apuesta * 2;
                         esperar(0.35);
                         System.out.println("Beneficio: +" + redondear(apuesta));
                         System.out.println("Saldo actual: " + redondear(saldo));
@@ -209,7 +223,7 @@ public class programa {
                     if (resultado == 1) {
 
                         System.out.println("HA GANADO !!!");
-                        saldo = apuesta * 2;
+                        saldo = saldo + apuesta * 2;
                         esperar(0.35);
                         System.out.println("Beneficio: +" + redondear(apuesta));
                         System.out.println("Saldo actual: " + redondear(saldo));
@@ -225,7 +239,7 @@ public class programa {
                     if (resultado == 2) {
 
                         System.out.println("HA GANADO !!!");
-                        saldo = apuesta * 35;
+                        saldo = saldo + apuesta * 35;
                         esperar(0.35);
                         System.out.println("Beneficio: +" + redondear(((apuesta * 35) / 2)));
                         System.out.println("Saldo actual: " + redondear(saldo));
@@ -241,13 +255,153 @@ public class programa {
                     System.out.println("Saliendo...");
                     salir = true;
                     break;
+                default:
+                    System.out.println("Opción inválida");
+                    esperar(0.5);
+                    break;
             }
             System.out.println("-------------------------------");
         } while (!salir);
         return saldo;
 
-
     }
+
+    public static double blackJackCartas(double apuesta) {
+
+        int random;
+        String opcion;
+        int min = 1;
+        int max = 11;
+        int manoC = random(max,min);
+        int manoU = random(max,min);
+        boolean salir;
+        boolean correcto;
+        do {
+            salir = false;
+            if (manoC < 17) {
+
+                System.out.println("Repartiendo cartas...");
+                esperar(1);
+                System.out.println("-------------------------------");
+                System.out.println("Tu mano: " + manoU);
+                System.out.println("Mano crupier: " + manoC);
+                System.out.println("-------------------------------");
+                esperar(0.5);
+
+            }
+
+            if (manoU == 21 && manoC == 21) {
+
+                System.out.println("Empate");
+                salir = true;
+                return apuesta;
+
+            } else if (manoU > 21) {
+
+                System.out.println("Ha perido...");
+                esperar(0.5);
+                salir = true;
+                return 0;
+
+            } else if ( manoC >= 17 && manoU <= 21) {
+
+
+                System.out.println("¡ HA GANADO !");
+                salir = true;
+                return apuesta * 2;
+
+            } else {
+
+                do {
+                    correcto = false;
+                    System.out.println("0 - Plantarse");
+                    System.out.println("1 - Pedir");
+                    opcion = leer.next();
+                    leer.nextLine();
+                    switch (opcion) {
+
+                        case "0":
+
+                            while (manoC < 17) {
+
+                                manoC = manoC + random(max,min);
+                                System.out.println("-------------------------------");
+                                System.out.println("Tu mano: " + manoU);
+                                System.out.println("Mano crupier: " + manoC);
+                                System.out.println("-------------------------------");
+                                esperar(0.6);
+
+                            }
+                            correcto = true;
+
+                            break;
+                        case "1":
+                            manoU = manoU + random(max,min);
+                            correcto = true;
+                            break;
+                        default:
+                            System.out.println("Opción inválida");
+                            break;
+                    }
+
+                } while (!correcto);
+
+            }
+
+        } while (!salir);
+
+        return 0;
+    }
+
+    public static double blackJack(double saldo) {
+
+        boolean salir;
+        boolean correcto;
+        double apuesta;
+        String opcion;
+        System.out.println("♣ Bienvenido al Black Jack ♣");
+        esperar(0.7);
+        System.out.println("Aquí estan usted, el destino y la fortuna.");
+        esperar(1);
+        do {
+
+            salir = false;
+            apuesta = apuesta(saldo);
+            if (apuesta == 0) {
+
+                salir = true;
+                break;
+
+            }
+            saldo = saldo - apuesta;
+            saldo = saldo + blackJackCartas(apuesta);
+            System.out.println("Saldo restante: " + redondear(saldo));
+            do {
+                correcto = false;
+                System.out.println("¿Quiere jugar de nuevo? (S/N)");
+                opcion = leer.next();
+                leer.nextLine();
+                switch (opcion) {
+
+                    case "S","s":
+                        correcto = true;
+                        break;
+                    case "N","n":
+                        correcto = true;
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Opción inválida");
+                        break;
+                }
+
+            } while (!correcto);
+
+        } while (!salir);
+
+        return saldo;
+    }
+
 
     public static void main(String[] args) {
 
@@ -282,7 +436,7 @@ public class programa {
                     break;
 
                 case "2":
-                    System.out.println("BlackJack");
+                    saldo = blackJack(saldo);
                     break;
 
                 case "3":
