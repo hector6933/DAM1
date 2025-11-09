3d8c15d040baed278db497c6c607bb240a60969b
hector6933 <hcriadog@iesch.org>
2025-11-09 23:42:33 +0100

    push por si acaso

---
+++ Java_Intellij/Hector-Pruebas/probatinasCasa/src/probatina1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class probatina1 {
+
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+        String input1;
+        System.out.println("Hola1");
+        System.out.print("Hola2");
+        input1 = leer.next();
+        leer.nextLine();
+        System.out.print("Hola3");
+
+        leer.close();
+
+
+    }
+
+}
