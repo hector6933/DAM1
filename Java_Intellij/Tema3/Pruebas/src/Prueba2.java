b0c418ab00b0cad60ccccfc6c5c1939acf84758b
hector6933 <hcriadog@iesch.org>
2025-11-10 21:13:05 +0100

    Unas pruebas de java

---
+++ Java_Intellij/Tema3/Pruebas/src/Prueba2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Prueba2 {
+
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+
+        int numerin;
+        boolean correcto = false;
+
+        do {
+
+            System.out.println("Dime un número: ");
+            System.out.print("> ");
+
+            try {
+
+                numerin = Integer.parseInt(leer.nextLine());
+                correcto = true;
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("¡ Valor inválido !");
+
+            }
+            
+            /*
+
+            if (leer.hasNextInt()) {
+
+                numerin = leer.nextInt();
+                leer.nextLine();
+                correcto = true;
+
+            } else {
+
+                System.out.println("Eso no es un entero");
+                leer.nextLine();
+
+            }
+            */
+
+        } while (!correcto);
+
+    }
+
+}
