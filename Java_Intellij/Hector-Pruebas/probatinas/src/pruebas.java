e32629753412e05c0746236455a77a18f9018a71
hector6933 <hcriadog@iesch.org>
2025-11-13 17:30:59 +0100

    por si acaso

---
+++ Java_Intellij/Hector-Pruebas/probatinas/src/pruebas.java
@@ -0,0 +1,13 @@
+public class pruebas {
+    public static void main(String[] args) {
+        int num1 = 3, num2 = 4;
+
+        boolean flag = true;
+
+        flag = (17 == 13);
+
+        System.out.println(flag);
+
+
+    }
+}
