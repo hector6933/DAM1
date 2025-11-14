756c2dbc9a51ee1e2b4446aeab2bc4df877cb0d4
hector6933 <hcriadog@iesch.org>
2025-11-14 12:10:31 +0100

    cositas

---
+++ Java_Intellij/Hector-Pruebas/probatinasCasa/src/probatina2.java
@@ -0,0 +1,12 @@
+public class probatina2 {
+
+    public static void main() {
+
+        int num1 = 0;
+        int num2 = new Integer(33);
+
+        System.out.println(num1 == num2);
+
+    }
+
+}
---
+++ Java_Intellij/Hector-Pruebas/probatinasCasa/src/probatina3.java
@@ -0,0 +1,11 @@
+public class probatina3 {
+
+    public static void main() {
+
+        double a = .79;
+        double b = 100;
+        System.out.println(a*b);
+
+    }
+
+}
