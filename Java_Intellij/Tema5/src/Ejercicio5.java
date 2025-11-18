0ed877ff6f88c9870eff3ca43eff967997632e4d
hector6933 <hcriadog@iesch.org>
2025-11-18 21:15:48 +0100

    Terminado el ejercicio 5 de arrays de programación

--- Java_Intellij/Tema5/src/Ejercicio5.java
+++ Java_Intellij/Tema5/src/Ejercicio5.java
@@ -6,23 +6,60 @@ public class Ejercicio5 {
         Scanner leer = new Scanner(System.in);
 
         int[] randoms = new int[10];
-
         int pos = 0;
+        boolean esta;
+        boolean correcto;
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < randoms.length; i++) {
 
             randoms[i] = (int) (Math.random() * 100);
 
         }
 
+        System.out.println("---------------------------");
+        System.out.println("ARRAY:");
+        for (int a : randoms) {
+
+            System.out.println(a);
+
+        }
+        System.out.println("---------------------------");
+
         do {
+            correcto = false;
             System.out.println("Introduce un número entero y te digo si está en el array (Numero negativo para salir)");
+            System.out.print("> ");
             try {
 
                 pos = Integer.parseInt(leer.nextLine());
+                correcto = true;
                 if (pos >= 0) {
 
+                    esta = false;
 
+                    for (int random : randoms) {
+
+                        if (pos == random) {
+
+                            esta = true;
+                            break;
+
+                        }
+
+                    }
+                    if (esta) {
+
+                        System.out.println("El número se encuentra en el Array");
+
+                    } else {
+
+                        System.out.println("El número NO se encuentra en el Array !!!");
+
+                    }
+
+                } else {
+
+                    System.out.println("Saliendo...");
 
                 }
 
@@ -32,8 +69,7 @@ public class Ejercicio5 {
 
             }
 
-
-        } while (pos > 0);
+        } while (pos >= 0 || !correcto);
 
     }
 
