5952825cd620f247f8b97e4c7f583f83520f14f7
hector6933 <hcriadog@iesch.org>
2025-11-28 18:21:54 +0100

    Cosas de SQL

--- BasesDeDatos/SQL/Ejercicio2Tema5.sql
+++ BasesDeDatos/SQL/Ejercicio2Tema5.sql
@@ -36,3 +36,64 @@ CREATE TABLE alumnos (
 	CONSTRAINT ck_curso CHECK (curso>=1 AND curso<=3),
 	CONSTRAINT pk_codigo PRIMARY KEY (codigo)
 );
+
+CREATE TABLE empleado(
+	cod_empleado NUMERIC(2),
+	nombre VARCHAR(20) NOT NULL,
+	apellido VARCHAR(25),
+	salario DOUBLE(7,2),
+	provincia NUMERIC(2),
+	empresa NUMERIC(2),
+	CONSTRAINT pk_cod_empleado PRIMARY KEY (cod_empleado),
+	CONSTRAINT ck_salario_mayor0 CHECK (salario>0),
+	CONSTRAINT fk_emp_provincia FOREIGN KEY (provincia) REFERENCES provincias (cod_provi) ON DELETE CASCADE,
+	CONSTRAINT fk_emp_empresa FOREIGN KEY (empresa) REFERENCES empresas (cod_empresa)
+);
+
+CREATE TABLE personas(
+	dni CHAR(9),
+	nombre VARCHAR(20),
+	direccion VARCHAR(100),
+	codprovin NUMERIC(2),
+	CONSTRAINT fk_codprovin FOREIGN KEY (codprovin) REFERENCES provincias (cod_provi) ON DELETE CASCADE,
+	CONSTRAINT pk_dni PRIMARY KEY (dni)
+);
+
+CREATE TABLE ejemplo1 (
+	dni VARCHAR(10),
+	nombre VARCHAR(30),
+	edad NUMERIC(2),
+	fecha DATE,
+	CONSTRAINT pk_dni PRIMARY KEY (dni)
+);
+
+CREATE TABLE ejemplo2 (
+	dni VARCHAR(10),
+	nombre VARCHAR(30),
+	edad NUMERIC(2),
+	curso NUMERIC,
+	CONSTRAINT ck_edad CHECK (edad>=5 AND edad<=20),
+	CONSTRAINT ck_curso_ejemplo3 CHECK (curso IN (1,2,3))
+);
+
+CREATE TABLE alumnos2 (
+	codigo NUMERIC(2),
+	nombre VARCHAR(25) NOT NULL,
+	CONSTRAINT codigo PRIMARY KEY (CODIGO)
+);
+
+CREATE TABLE modulos (
+	codigo INTEGER,
+	nombre VARCHAR(25),
+	CONSTRAINT pk_codigo_modulos PRIMARY KEY (codigo)
+
+);
+-- Comentario
+CREATE TABLE notas (
+	cod_alumno NUMERIC(2),
+	cod_modulo INTEGER,
+	nota NUMERIC(2),
+	CONSTRAINT pk_cod_al_mod PRIMARY KEY (cod_alumno,cod_modulo),
+	CONSTRAINT fk_cod_modulo_notas FOREIGN KEY (cod_modulo) REFERENCES modulos (codigo),
+	CONSTRAINT fk_cod_alumnos_notas FOREIGN KEY (cod_alumno) REFERENCES alumnos2 (codigo)
+);
