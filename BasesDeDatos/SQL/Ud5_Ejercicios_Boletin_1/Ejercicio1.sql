65e0d3c56196ec237368fd94dd1daf925c20d979
hector6933 <hcriadog@iesch.org>
2025-12-05 21:15:56 +0100

    por si acaso

--- BasesDeDatos/SQL/Ud5_Ejercicios_Boletin_1/Ejercicio1.sql
+++ BasesDeDatos/SQL/Ud5_Ejercicios_Boletin_1/Ejercicio1.sql
@@ -73,12 +73,14 @@ código de especialidad 999. ¿Qué ocurre? ¿Por qué?
 */
 -- Esa insercción da un error ya que la especialidad 999 no existe
 -- Así está insertado correctamente:
+
 INSERT INTO medicos (num_colegiado,nombre,apellidos,email,anos_experiencia,cod_especialidad) VALUES ('28-4567890','Carlos','Fernández Ruiz','cfernandez@clinica.es',5,101);
 
 /*
 Inserta en PACIENTES un paciente con DNI '12345678A', nombre 'María', apellidos 'García
 López', fecha de nacimiento '1990-05-15', grupo sanguíneo 'A+' y teléfono '666777888'.
 */
+
 INSERT INTO pacientes (dni,nombre,apellidos,fecha_nacimiento,grupo_sanguineo,telefono_contacto) VALUES ('12345678A','María','García López','1990-06-15','A+','666777888');
 
 /*
@@ -86,5 +88,39 @@ Inserta en CONSULTAS una consulta del paciente anterior con el médico Carlos Fe
 precio 75.50 y estado de pago 'N'.
 */
 
-
 INSERT INTO consultas (num_historial_paciente,num_colegiado_medico,precio,pagado) VALUES (1,'28-4567890',75.50,'N');
+
+/*
+Añade un campo horario_consulta en la tabla MEDICOS que sea de tipo texto de 20
+caracteres (valores permitidos: 'MAÑANA', 'TARDE', 'COMPLETO').
+*/
+
+ALTER TABLE medicos ADD horario_consulta VARCHAR(20);
+ALTER TABLE medicos ADD CONSTRAINT ck_horario_consulta CHECK (horario_consulta IN ('MAÑANA','TARDE','COMPLETO'));
+
+/*
+Modifica el campo email de la tabla MEDICOS para que tenga una longitud de 100
+caracteres.
+*/
+
+ALTER TABLE medicos MODIFY email VARCHAR(100);
+
+/*
+Añade un campo mutua en la tabla PACIENTES de tipo texto de 50 caracteres con valor por
+defecto 'PARTICULAR'
+*/
+
+ALTER TABLE pacientes ADD mutua VARCHAR(50) DEFAULT 'PARTICULAR';
+
+/*
+Elimina el campo alergias de la tabla PACIENTES
+*/
+
+ALTER TABLE pacientes DROP alergias;
+
+/*
+Añade una restricción CHECK a la tabla CONSULTAS para que el campo precio no pueda
+superar los 1000 euros (modifica la restricción existente).
+*/
+
+ALTER TABLE MODIFY ck_precio_consultas ADD CHECK (precio>=0 AND precio<1000);
