b7dcbaf40a5541ca506cdd06cf0759e3a49cd507
hector6933 <hcriadog@iesch.org>
2025-12-13 15:44:30 +0100

    cosas piton

---
+++ Python_Intellij/primerPaquete/probatinas/circulos.py
@@ -0,0 +1,27 @@
+from turtle import *
+speed(9999999999999999)
+color("red")
+contador = 1
+esferas = 10
+while True:
+    begin_fill()
+    circle(100)
+    end_fill()
+    forward(100)
+    contador += 1
+    if contador % 2 == 0:
+        color("black")
+
+    else:
+        color("red")
+    if contador == esferas+1:
+        color("blue")
+        left(90)
+        forward(200)
+        left(90)
+        forward((esferas+1)*100)
+        left(90)
+        forward(200)
+        left(90)
+        forward((esferas+1)*100)
+        mainloop()
