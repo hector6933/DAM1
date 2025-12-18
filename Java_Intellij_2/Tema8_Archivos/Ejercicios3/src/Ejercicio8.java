8e515bfc21bcd6a6c04713fa16298364fd114071
hector6933 <hcriadog@iesch.org>
2025-12-18 18:16:43 +0100

    Skbidi sigma

--- Java_Intellij_2/Tema8_Archivos/Ejercicios3/src/Ejercicio8.java
+++ Java_Intellij_2/Tema8_Archivos/Ejercicios3/src/Ejercicio8.java
@@ -1,48 +1,18 @@
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Ejercicio8 {
 
     public static void main(String[] args) {
 
-        // POR HACER, ARREGLAR EL OUT OF BOUNDS
-        try (BufferedReader leer = new BufferedReader(new FileReader("carpeta/Ej8/Archivo.txt"))) {
+        try {
 
-            String linea;
-            int palabras = 0;
+            List<String> lineas = Files.readAllLines(Paths.get("carpeta/Ej8/Archivo.txt"));
 
-            while ( (linea = leer.readLine()) != null) {
-
-                int i = 0;
-
-
-                while (linea.charAt(i) == ' ' ) {
-
-                    i++;
-
-                }
-
-                for ( ; i < linea.length(); i++) {
-
-                    if (linea.charAt(i) == ' ') {
-
-                        palabras++;
-                        while (linea.charAt(i+1) == ' ' ) {
-
-                            i++;
-
-                        }
-
-                    }
-
-                }
-
-
-            }
-
-            System.out.println("Hay un total de " + palabras + " palabras");
+            String[] palabras = lineas.toArray(new String[0]);
 
         } catch (FileNotFoundException e) {
 
