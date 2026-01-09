4f502fff2b22179817b3a5401e16c642bd138eac
hector6933 <hcriadog@iesch.org>
2026-01-09 19:26:47 +0100

    AAAAAAAAAAAAA

---
+++ BasesDeDatos/SQL/Ud5_Ejercicios_Boletin_1/Ejercicio2.sql
@@ -0,0 +1,100 @@
+-- Ejercicio 2
+
+-- Borramos la base de datos
+DROP DATABASE IF EXISTS ACADEMIA_VIRTUAL;
+
+-- Creamos la base de datos
+CREATE DATABASE ACADEMIA_VIRTUAL;
+
+-- Usamos la base de datos
+USE ACADEMIA_VIRTUAL;
+
+-- Creamos la tabla categoria
+CREATE TABLE categorias(
+	id_categoria INTEGER AUTO_INCREMENT,
+	nombre_categoria VARCHAR(40) UNIQUE NOT NULL,
+	descripcion VARCHAR(150),
+	activa CHAR(1) DEFAULT 'S',
+	CONSTRAINT pk_id_categoria PRIMARY KEY (id_categoria),
+	CONSTRAINT ck_activa CHECK (activa IN ('S','N'))
+);
+
+-- Creamos la tabla instructores
+CREATE TABLE instructores (
+	email VARCHAR(100),
+	nombre_completo VARCHAR(80) NOT NULL,
+	biografia TEXT,
+	fecha_registro DATETIME DEFAULT NOW(),
+	puntuacion_media DECIMAL(3,2),
+	certificado CHAR(1),
+	linkedin VARCHAR(150),
+	CONSTRAINT ck_certificado CHECK (certificado IN ('S','N')),
+	CONSTRAINT ck_puntuacion_media CHECK(puntuacion_media>=0 AND puntuacion_media<=5),
+	CONSTRAINT pk_email PRIMARY KEY (email)
+);
+
+-- Creamos la tabla cursos
+
+CREATE TABLE cursos(
+	cod_curso VARCHAR(10),
+	titulo VARCHAR(100) NOT NULL,
+	descripcion TEXT,
+	nivel VARCHAR(15),
+	duracion_horas INT,
+	precio DECIMAL(6,2),
+	fecha_creacion DATETIME DEFAULT NOW(),
+	plaza_max INT,
+	id_categoria INTEGER,
+	email_instructor VARCHAR(100),
+	CONSTRAINT fk_email_instructor_cursos FOREIGN KEY (email_instructor) REFERENCES instructores (email) ON DELETE CASCADE,
+	CONSTRAINT fk_id_categoria_cursos FOREIGN KEY (id_categoria) REFERENCES categorias (id_categoria) ON DELETE RESTRICT,
+	CONSTRAINT ck_plazas_max CHECK (plaza_max>0 AND plaza_max<=100),
+	CONSTRAINT ck_precio CHECK (precio>=0),
+	CONSTRAINT ck_duracion_horas CHECK (duracion_horas>0 AND duracion_horas <500),
+	CONSTRAINT ck_nivel CHECK (nivel IN ('BASICO','INTERMEDIO','AVANZADO','EXPERTO')),
+	CONSTRAINT pk_cod_curso PRIMARY KEY (cod_curso)
+);
+
+-- Creamos la tabla estudiantes
+
+CREATE TABLE estudiantes(
+	id_estudiante INT AUTO_INCREMENT,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	nombre VARCHAR(40) NOT NULL,
+	apellidos VARCHAR(60) NOT NULL,
+	pais VARCHAR(50),
+	fecha_nacimiento DATE,
+	premiun CHAR(1) DEFAULT 'N',
+	CONSTRAINT ck_premiun CHECK (premiun IN ('S','N')),
+	CONSTRAINT pk_id_estudiante PRIMARY KEY (id_estudiante)
+);
+
+-- Creamos la tabla matriculas
+
+CREATE TABLE matriculas(
+	id_matricula INT AUTO_INCREMENT,
+	id_estudiante INT,
+	cod_curso VARCHAR(10),
+	fecha_matricula DATETIME DEFAULT NOW(),
+	progreso INT DEFAULT 0,
+	calificacion_final DECIMAL(4,2),
+	completado CHAR(1) DEFAULT 'N',
+	CONSTRAINT ck_completado CHECK (completado IN ('S','N')),
+	CONSTRAINT ck_calificacion CHECK (calificacion_final>=0 AND calificacion_final<=100),
+	CONSTRAINT ck_progreso CHECK (progreso>=0 AND progreso<=100),
+	CONSTRAINT fk_cod_curso_matriculas FOREIGN KEY (cod_curso) REFERENCES cursos (cod_curso) ON DELETE RESTRICT,
+	CONSTRAINT fk_id_estudiante_matriculas FOREIGN KEY (id_estudiante) REFERENCES estudiantes (id_estudiante) ON DELETE CASCADE,
+	CONSTRAINT pk_id_matricula PRIMARY KEY (id_matricula),
+	-- RESTRICCION ADICIONAL
+	CONSTRAINT uq_curso_matric UNIQUE(id_matricula, cod_curso)
+);
+
+-- Inserto una categoria 
+INSERT INTO categorias (nombre_categoria,descripcion,activa) VALUES ('PROGRAMACION','Categoria de programacion','S');
+
+-- Inserto un instructor
+INSERT INTO instructores (email,nombre_completo,puntuacion_media,certificado) VALUES ('juan.perez@academia.com','Juan Pérez Sánchez',4.75,'S');
+
+-- Inserto un curso
+
+
