b95456290042f3d4a9745319b529b2421e9afc60
hector6933 <hcriadog@iesch.org>
2026-01-09 17:30:42 +0100

    Programacion (hay que acabar el 8)

--- Java_Intellij_2/Tema8_Archivos/Ejercicios3/src/Ejercicio8.java
+++ Java_Intellij_2/Tema8_Archivos/Ejercicios3/src/Ejercicio8.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 public class Ejercicio8 {
@@ -13,6 +14,50 @@ public class Ejercicio8 {
             List<String> lineas = Files.readAllLines(Paths.get("carpeta/Ej8/Archivo.txt"));
 
             String[] palabras = lineas.toArray(new String[0]);
+
+            int contadorPalabras = 0;
+
+            for (int i = 0; i < palabras.length; i++) {
+                int pos = 0;
+
+                if (palabras[i].charAt(pos) == ' ') {
+
+                    while (palabras[i].charAt(pos) == ' ') {
+
+                        pos++;
+
+                    }
+
+                }
+
+                for (int j = 0; j < palabras[i].length(); j++) {
+
+                    if (palabras[i].charAt(pos) == ' ') {
+
+                        while (palabras[i].charAt(pos) == ' ') {
+
+                            pos++;
+
+                        }
+
+                        contadorPalabras++;
+
+                    } else if (j == palabras[i].length()-1) {
+
+                        contadorPalabras++;
+
+
+                    } else {
+
+                        pos++;
+
+                    }
+
+                }
+
+            }
+
+            System.out.println("El texto tiene " + contadorPalabras + " palabras");
 
         } catch (FileNotFoundException e) {
 
