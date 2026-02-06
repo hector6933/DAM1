ac843587c769569df0f3ee1a8220a02441ff466b
hector6933 <hcriadog@iesch.org>
2026-02-06 18:24:29 +0100

    Hecho hasta el 25 del ejercicio de ventas

--- BasesDeDatos/SQL/UD6/EjercicioConsultasSencillas.sql
+++ BasesDeDatos/SQL/UD6/EjercicioConsultasSencillas.sql
@@ -39,8 +39,27 @@ SELECT * FROM pedidos WHERE precio_total < ALL (SELECT precio_total FROM pedidos
 SELECT * FROM pedidos WHERE fecha_pedido < ALL (SELECT fecha_pedido FROM pedidos WHERE id_cliente = 1005);
 
 SELECT cli.nombre, cli.apellidos, ped.numero_pedido, ped.fecha_pedido FROM clientes cli NATURAL LEFT JOIN pedidos ped;
--- 21:
-SELECT ped.numero_pedido, pro.nombre, ped.cantidad
-FROM detalles_pedidos ped INNER JOIN productos pro 
-ON ped.numero_producto=pro.numero_producto;
 
+-- 21:
+SELECT ped.numero_pedido, pro.nombre, (SELECT cantidad FROM detalles_pedidos WHERE numero_producto=pro.numero_producto AND numero_pedido=ped.numero_pedido) AS 'Cantidad en pedido', pro.precio_venta AS 'Precio unitario'
+FROM detalles_pedidos ped INNER JOIN productos pro ON ped.numero_producto=pro.numero_producto WHERE ped.numero_pedido = (SELECT numero_pedido FROM pedidos WHERE fecha_pedido>='2008-01-01' AND numero_pedido=ped.numero_pedido) ORDER BY ped.numero_pedido;
+
+
+-- 22:
+SELECT cat.descripcion, (SELECT COUNT(*) FROM productos WHERE cat.id_categoria=id_categoria) AS 'Productos', (SELECT AVG(precio_venta) FROM productos WHERE cat.id_categoria=id_categoria) AS 'Precio promedio'
+FROM categorias cat;
+
+-- 23:
+SELECT prov.nombre, (SELECT nombre FROM productos WHERE prodprov.numero_producto=numero_producto), precio_por_mayor
+FROM proveedores prov NATURAL JOIN productos_proveedores prodprov WHERE prodprov.numero_producto IN (SELECT numero_producto FROM productos WHERE id_categoria IN (SELECT id_categoria FROM categorias WHERE descripcion='Bicicletas'));
+
+-- 24:
+SELECT (SELECT nombre FROM clientes WHERE ped.id_cliente=id_cliente) AS 'Cliente',emp.nombre AS 'Empleado', ped.fecha_pedido
+FROM pedidos ped NATURAL JOIN empleados emp WHERE ped.id_cliente = (SELECT id_cliente FROM clientes WHERE ciudad='Robledo' AND ped.id_cliente=id_cliente) AND ped.id_empleado = (SELECT id_empleado FROM empleados WHERE ciudad='Robledo' AND emp.id_empleado=id_empleado);
+
+-- 25:
+SELECT pro.* 
+FROM productos pro WHERE (SELECT COUNT(*) FROM detalles_pedidos WHERE numero_producto=pro.numero_producto) = 0;
+
+-- 26:
+SELECT 
