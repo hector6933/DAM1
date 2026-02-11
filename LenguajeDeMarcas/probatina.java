d67102b433f9f4e5bfc17a586f52b28a062efe10
hector6933 <hcriadog@iesch.org>
2026-02-11 13:45:01 +0100

    Hecho el inicio y leyes de la web asimov

---
+++ LenguajeDeMarcas/probatina.java
@@ -0,0 +1,14 @@
+public class probatina{
+
+    public static void main(String[] args){
+
+        for (int i = 0; i < 10; i++) {
+            
+            System.out.println("Hola que tal");
+
+        }
+        
+
+    }
+
+}
