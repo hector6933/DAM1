952b54c0047525bee9d86e11d660b7964e244903
hector6933 <hcriadog@iesch.org>
2026-02-17 13:34:19 +0100

    prueba tonta

---
+++ Java_Intellij_2/Tema11_GestionDinamicaDeLaMemoria/src/Probatinas/Sumar.java
@@ -0,0 +1,18 @@
+package Probatinas;
+
+public class Sumar {
+
+    static void main() {
+
+        int total = 0;
+        for (int i = 0; i <= 200; i++) {
+
+            total += i;
+
+        }
+
+        System.out.println("La suma total es de: " + total);
+
+    }
+
+}
