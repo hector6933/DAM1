d4a37865cad154fbb1c7772a7c037816bb3d1163
hector6933 <hcriadog@iesch.org>
2026-03-17 18:21:58 +0100

    Kotlin

--- Java_Intellij_3_Maven/src/main/java/bucle.java
+++ Java_Intellij_3_Maven/src/main/java/bucle.java
@@ -1,5 +1,12 @@
 public class bucle {
 
+
+    public static void saludar(String nombre) {
+
+        System.out.println("Hola " + nombre + " !");
+
+    }
+
     public static void main(String[] args) {
 
         while (true) {
