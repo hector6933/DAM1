f202951f84043f3e1223176578de9ce74092c08d
hector6933 <hcriadog@iesch.org>
2026-04-16 19:37:27 +0200

    Empezamos con VUE

---
+++ LenguajeDeMarcas/UD8/holaMundo/src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { setBlockTracking } from 'vue'
+
+const sout = (a) => console.log(a)
+
+const nombre = "Vue 3"
+
+const styleHr = "margin: 20px 0;"
+
+const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"]
+
+const descargaFrutas = [
+  {
+    name: "Manzana",
+    price: "1.00€",
+    description: "Manzana Golden",
+    stock: 0
+  },
+  {
+    name: "Pera",
+    price: "2.00€",
+    description: "Pera Conferencia",
+    stock: 10
+  },
+  {
+    name: "Naranja",
+    price: "3.00€",
+    description: "Naranjas de Valencia",
+    stock: 20
+  }
+]
+
+const fruta = {
+
+  name: "Fresa",
+  price: "4.00€",
+  description: "Fresón de Palos"
+
+}
+
+</script>
+
+<template>
+
+  <div style="padding: 20px;">
+
+    <h1>Hola {{ nombre }}</h1>
+    
+    <hr :style="styleHr">
+
+    <h1>Directiva v-for</h1>
+    <ul>
+      <li v-for="(fruta, index) in arrayFrutas" :key="index">
+        
+        {{ index }} - {{ fruta }}
+      </li>
+    </ul>
+
+    <hr :style="styleHr">
+
+    <h1>Recorremos un Array de objetos</h1>
+
+    <ul>
+      <li v-for="fruta in descargaFrutas" :key="fruta.name">
+          {{ fruta.name }} - {{ fruta.description }} - {{ fruta.price }}
+      </li>
+    </ul>
+
+    <hr :style="styleHr">
+
+    <h1>Recorremos un objeto</h1>
+
+    <ul>
+      <li v-for="(valor, propiedad, index) in fruta">
+       {{ index }} - {{ propiedad }} - {{ valor }}
+      </li>
+    </ul>
+
+    <hr :style="styleHr">
+
+    <h1>Lista de productos con 10 o más unidades</h1>
+    <ul>
+      <li v-for="(valor, propiedad, index) in fruta">
+       {{ index }} - {{ propiedad }} - {{ valor }}
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<style>
+  h1 {
+
+    color: white;
+    font-weight: bold;
+  
+  }
+</style>
---
+++ LenguajeDeMarcas/UD8/holaMundo/src/App2.vue
@@ -0,0 +1,75 @@
+<script setup>
+const sout = (a) => console.log(a)
+
+const colores = ['white','blue','red','green','yellow']
+
+/* const color = 'blue'
+
+const obtenerUsuarios = async () => {
+
+    while (true) {
+
+      colores.forEach( col => color = col)
+    
+
+    }
+
+}*/
+
+const nombre = "la clase de DAM 1"
+
+const styleColor = "color: white;"
+const styleBold = "font-weight: bold;"
+
+const styleHr = "margin: 20px 0;"
+
+const active = false
+
+
+</script>
+
+<template>
+
+  <div style="padding: 20px;">
+
+    <h1>Hola {{ nombre }}</h1>
+    <h2 :style="styleColor">Hola {{ nombre }}</h2>
+
+    <hr :style="styleHr">
+
+    <h2>Hola {{ nombre.toUpperCase() }}</h2>
+
+    <hr :style="styleHr">
+
+    <p>{{ active ? 'Estoy activo' : 'Estoy inactivo' }}</p>
+
+    <h3 :style="`color: ${colores[1]}`">Color con interpolación</h3>
+
+    <hr :style="styleHr">
+
+    <h1>Directiva v-if</h1> <!-- v-if lo pone o no lo pone-->
+    <h2 v-if="active">Active vale true</h2> <!-- Muestra algo o no dependiendo del valor booleano proporcionado-->
+    <h2 v-else-if="activa === false">Active vale false</h2>
+    <h2 v-else>No se sabe lo que vale active</h2> <!-- Para poder usar el v-else el elemento anterior debe de ser un v-if, si no da fallo -->
+    <!-- <h2 v-if="!active">Active vale false</h2> otra manera -->
+
+    <hr :style="styleHr">
+
+    <h1>Directiva v-show</h1> <!-- Si que lo pone pero no lo muestra -->
+
+    <h2 v-show="active">Active vale true</h2>
+
+  </div>
+  
+</template>
+
+
+
+<style>
+  h1 {
+
+    color: white;
+    font-weight: bold;
+  
+  }
+</style>
