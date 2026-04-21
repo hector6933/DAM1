85abf0f730690e58e7ebba0f829a011e640ba1a5
hector6933 <hcriadog@iesch.org>
2026-04-21 21:11:58 +0200

    Cosas de interfazes arreglar el inicio de sesión

--- AplicacionConcesionarioHector/src/main/java/Vista/Main.java
+++ AplicacionConcesionarioHector/src/main/java/Vista/Main.java
@@ -13,13 +13,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int x = 400;
+        int y = 400;
+
         JFrame frame = new JFrame("Login");
+        frame.setSize(x, y);
         frame.setLocationRelativeTo(null);
-        frame.setLocation(725,325);
+        frame.setLocation( (int) frame.getLocation().getX()-(x/2) ,(int) frame.getLocation().getY()-(y/2));
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
         frame.setLayout(new GridLayout(3,1));
-        frame.setResizable(false);
+        frame.setResizable(true);
 
         // -------------------- INICIO TEXTO INICIAR SESIÓN ----------------------
 
@@ -42,65 +46,74 @@ public class Main {
 
         // -------------------- INICIO USERNAME -------------------------------
 
-        JPanel panelLogUsername = new JPanel(new GridLayout(2,1));
+        JPanel panelLogUsername = new JPanel(new GridLayout(1,1));
 
-        JPanel panelTextoUsername = new JPanel(sLayout);
+        JPanel panelLogin = new JPanel(sLayout);
 
         JLabel textoUsername = new JLabel("Usuario");
 
         textoUsername.setFont(new Font("JetBrains Mono",Font.BOLD,20));
 
-        panelTextoUsername.add(textoUsername);
+        panelLogin.add(textoUsername);
 
         sLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, textoUsername, -77,
-                SpringLayout.HORIZONTAL_CENTER, panelTextoUsername);
+                SpringLayout.HORIZONTAL_CENTER, panelLogin);
         sLayout.putConstraint(SpringLayout.NORTH, textoUsername, 0,
-                SpringLayout.NORTH, panelTextoUsername);
+                SpringLayout.NORTH, panelLogin);
 
         campoUsername = new JTextField(20);
-        panelTextoUsername.add(campoUsername);
+        panelLogin.add(campoUsername);
 
         sLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, campoUsername, 185,
                 SpringLayout.HORIZONTAL_CENTER, panelLogUsername);
         sLayout.putConstraint(SpringLayout.NORTH, campoUsername, 35,
                 SpringLayout.NORTH, panelLogUsername);
 
-        panelLogUsername.add(panelTextoUsername);
+        panelLogUsername.add(panelLogin);
 
         // -------------------- FIN USERNAME ----------------------------------
 
         // -------------------- INICIO PASSWD ---------------------------------
 
-        JPanel panelLogPasswd = new JPanel(new GridLayout(2,1));
-
-        JPanel panelTextoPasswd = new JPanel(sLayout);
-
         JLabel textoPasswd = new JLabel("Contraseña");
 
         textoPasswd.setFont(new Font("JetBrains Mono",Font.BOLD,20));
 
-        panelTextoPasswd.add(textoPasswd);
+        panelLogin.add(textoPasswd);
 
         sLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, textoPasswd, -60,
-                SpringLayout.HORIZONTAL_CENTER, panelTextoPasswd);
-        sLayout.putConstraint(SpringLayout.NORTH, textoPasswd, 0,
-                SpringLayout.NORTH, panelTextoPasswd);
+                SpringLayout.HORIZONTAL_CENTER, panelLogin);
+        sLayout.putConstraint(SpringLayout.NORTH, textoPasswd, 70,
+                SpringLayout.NORTH, panelLogin);
 
         campoPasswd = new JPasswordField(20);
-        panelTextoPasswd.add(campoPasswd);
+        panelLogin.add(campoPasswd);
 
         sLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, campoPasswd, -8,
-                SpringLayout.HORIZONTAL_CENTER, panelTextoPasswd);
-        sLayout.putConstraint(SpringLayout.NORTH, campoPasswd, 35,
-                SpringLayout.NORTH, panelTextoPasswd);
-
-        panelLogPasswd.add(panelTextoPasswd);
+                SpringLayout.HORIZONTAL_CENTER, panelLogin);
+        sLayout.putConstraint(SpringLayout.NORTH, campoPasswd, 100,
+                SpringLayout.NORTH, panelLogin);
 
         // -------------------- FIN PASSWD ---------------------------------
 
+        // -------------------- INICIO BOTON -------------------------------
+
+        JPanel panelBoton = new JPanel(sLayout);
+
+        JButton enviarLogin = new JButton("Enviar");
+
+        panelBoton.add(enviarLogin);
+
+        sLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, enviarLogin, 0,
+                SpringLayout.HORIZONTAL_CENTER, panelLogin);
+        sLayout.putConstraint(SpringLayout.SOUTH, enviarLogin, 0,
+                SpringLayout.NORTH, panelLogin);
+
+        // -------------------- FIN BOTON -------------------------------
+
         frame.add(panelRow1);
         frame.add(panelLogUsername);
-        frame.add(panelLogPasswd);
+        frame.add(panelBoton);
 
         frame.setVisible(true);
 
